cb867dfafd124e2c9e9cbc2dd06f16a59e1f1737
Paul <paulgb3@gmail.com>
2012-02-28 18:17:45 -0600

    Fixed database, cleaned code in java fiels

--- src/edu/illinois/geoalarm/GeoAlarm.java
+++ src/edu/illinois/geoalarm/GeoAlarm.java
@@ -6,73 +6,47 @@ import android.app.Activity;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Spinner;
-import android.widget.Toast;
 
+public class GeoAlarm extends Activity {
 
-public class GeoAlarm extends Activity
-{
-	
 	GeoAlarmDB database;
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
 
-        // Instantiate the database
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// Instantiate the database
 		database = new GeoAlarmDB(this.getApplicationContext());
 
 		// Check the custom SQLite helper functions that load existing DB
-		try
-		{
+		try {
 			database.createDataBase();
-		}
-		catch (IOException e)
-		{
+		} catch (IOException e) {
 			throw new Error("Unable to create/find database");
 		}
 
 		// Open the SQLite database
-		try
-		{
+		try {
 			database.openDataBase();
-		}
-		catch (SQLException sql)
-		{
+		} catch (SQLException sql) {
 			throw new Error("Unable to execute sql in: " + sql.toString());
 		}
+	}
 
-		Spinner type = (Spinner) findViewById(R.id.spinner1);
-		type.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
-		{	
-			public void onItemSelected(AdapterView<?> parent, View view,
-					int pos, long id) {
-				Toast.makeText(parent.getContext(), "The planet is " +
-				          parent.getItemAtPosition(pos).toString(), Toast.LENGTH_LONG).show();
-				
-			}
-
-			public void onNothingSelected(AdapterView<?> arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		
+	void executeSomeQuery() {
 		// Execute SQLite to retrieve lines
-		Cursor theCursor = database.geoAlarmDB.query("Routes", null, null, null, null, null, null);
+		Cursor theCursor = database.geoAlarmDB.query("Routes", null, null,
+				null, null, null, null);
 
-		if(theCursor != null)
-		{
+		if (theCursor != null) {
 			theCursor.moveToFirst();
-					
+
 			String[] lineNames = theCursor.getColumnNames();
-					
-			for(int i = 0; theCursor.isAfterLast() != false; i++)
-			{
+
+			for (int i = 0; theCursor.isAfterLast() != false; i++) {
 				int nameColumn = theCursor.getColumnIndex("name");
 				lineNames[i] = theCursor.getString(nameColumn);
 
@@ -81,8 +55,6 @@ public class GeoAlarm extends Activity
 		}
 
 		theCursor.close();
-		database.geoAlarmDB.close();    
-		
-    }
-   
+		database.geoAlarmDB.close();
+	}
 }
--- src/edu/illinois/geoalarm/GeoAlarmDB.java
+++ src/edu/illinois/geoalarm/GeoAlarmDB.java
@@ -14,10 +14,10 @@ import android.database.sqlite.SQLiteOpenHelper;
 /**
  * @author GeoAlaem
  * 
- * GeoAlarmDB dynamically loads an existing database from the assets folder.
+ *         GeoAlarmDB dynamically loads an existing database from the assets
+ *         folder.
  */
-public class GeoAlarmDB extends SQLiteOpenHelper
-{
+public class GeoAlarmDB extends SQLiteOpenHelper {
 
 	private static final int DATABASE_VERSION = 1;
 	// Android's default system path for databases
@@ -30,59 +30,53 @@ public class GeoAlarmDB extends SQLiteOpenHelper
 
 	/**
 	 * Constructor
-	 *
+	 * 
 	 * @param context
 	 */
-	public GeoAlarmDB(Context context)
-	{
+	public GeoAlarmDB(Context context) {
 		super(context, DB_NAME, null, 1);
 		this.myContext = context;
 	}
 
-	// Creates an empty database on the system and rewrites it with the specified database table
-	public void createDataBase() throws IOException
-	{
+	// Creates an empty database on the system and rewrites it with the
+	// specified database table
+	public void createDataBase() throws IOException {
 		boolean dbExist = checkDataBase();
 
-		if (dbExist)
-		{
+		if (dbExist) {
 			// do nothing - database already exist, which is what we want
-		}
-		else
-		{
-			// By calling this method an empty database will be created into the default system path
-			// of the application so application will be able to overwrite that database with our database
+		} else {
+			// By calling this method an empty database will be created into the
+			// default system path
+			// of the application so application will be able to overwrite that
+			// database with our database
 			this.getReadableDatabase();
-			try
-			{
+			try {
 				copyDataBase();
-			}
-			catch (IOException e)
-			{
+			} catch (IOException e) {
 				throw new Error("Error copying database");
 			}
 		}
 	}
 
 	/**
-	 * Check if the database already exist to avoid re-copying the file each time user opens the application.
+	 * Check if the database already exist to avoid re-copying the file each
+	 * time user opens the application.
 	 * 
 	 * @return true if db exists, false otherwise
 	 */
-	private boolean checkDataBase()
-	{
+	private boolean checkDataBase() {
 		SQLiteDatabase checkDB = null;
 
-		try
-		{
+		try {
 			String myPath = DB_PATH + DB_NAME;
-			checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
-		}
-		catch(SQLiteException e) {
-			//database does't exist yet
+			checkDB = SQLiteDatabase.openDatabase(myPath, null,
+					SQLiteDatabase.OPEN_READONLY);
+		} catch (SQLiteException e) {
+			// database does't exist yet
 		}
 
-		if(checkDB != null) {
+		if (checkDB != null) {
 			checkDB.close();
 		}
 
@@ -90,15 +84,15 @@ public class GeoAlarmDB extends SQLiteOpenHelper
 	}
 
 	/**
-	 * Copies database from local assets-folder to the just created empty database in the
-	 * system folder, from where it can be accessed and handled.
+	 * Copies database from local assets-folder to the just created empty
+	 * database in the system folder, from where it can be accessed and handled.
 	 * This is done by transferring byte stream.
 	 * 
 	 * We followed the outline on:
-	 * http://www.reigndesign.com/blog/using-your-own-sqlite-database-in-android-applications/
+	 * http://www.reigndesign.com/blog/using-your-own
+	 * -sqlite-database-in-android-applications/
 	 */
-	private void copyDataBase() throws IOException
-	{
+	private void copyDataBase() throws IOException {
 		// Open your local db as the input stream
 		InputStream myInput = myContext.getAssets().open(DB_NAME);
 
@@ -107,12 +101,11 @@ public class GeoAlarmDB extends SQLiteOpenHelper
 
 		// Open the empty db as the output stream
 		OutputStream myOutput = new FileOutputStream(outFileName);
- 
+
 		// Transfer bytes from the input file to the output file
 		byte[] buffer = new byte[1024];
 		int length;
-		while ((length = myInput.read(buffer)) > 0)
-		{
+		while ((length = myInput.read(buffer)) > 0) {
 			myOutput.write(buffer, 0, length);
 		}
 
@@ -123,50 +116,44 @@ public class GeoAlarmDB extends SQLiteOpenHelper
 	}
 
 	/**
-	 * Opens the database from existing SQLite database file and loads it into geoAlarmDB global
+	 * Opens the database from existing SQLite database file and loads it into
+	 * geoAlarmDB global
 	 * 
 	 * @throws SQLException
 	 */
-	public void openDataBase() throws SQLException
-	{
+	public void openDataBase() throws SQLException {
 		// Open the database
 		String myPath = DB_PATH + DB_NAME;
-		geoAlarmDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+		geoAlarmDB = SQLiteDatabase.openDatabase(myPath, null,
+				SQLiteDatabase.OPEN_READONLY);
 	}
 
 	@Override
-	public synchronized void close()
-	{
-		if(geoAlarmDB != null)
-		{
+	public synchronized void close() {
+		if (geoAlarmDB != null) {
 			geoAlarmDB.close();
 		}
 
 		super.close();
 	}
 
-	public Context getMyContext()
-	{
+	public Context getMyContext() {
 		return myContext;
 	}
 
-	public static String getDB_NAME()
-	{
+	public static String getDB_NAME() {
 		return DB_NAME;
 	}
 
-	public static void setDB_NAME(String dB_NAME)
-	{
+	public static void setDB_NAME(String dB_NAME) {
 		DB_NAME = dB_NAME;
 	}
 
-	public SQLiteDatabase getGeoAlarmDB()
-	{
+	public SQLiteDatabase getGeoAlarmDB() {
 		return geoAlarmDB;
 	}
 
-	public void setProfessorDroidDB(SQLiteDatabase geoAlarmDB)
-	{
+	public void setProfessorDroidDB(SQLiteDatabase geoAlarmDB) {
 		this.geoAlarmDB = geoAlarmDB;
 	}
 
