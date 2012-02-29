e6b8ff51da670689e06c3553b2f4cba435d33197
Christopher DeFlumeri <deflume1@gmail.com>
2012-02-28 19:58:20 -0600

    all the methods

--- src/edu/illinois/geoalarm/TripPlannerBus.java
+++ src/edu/illinois/geoalarm/TripPlannerBus.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import android.app.Activity;
 import android.database.SQLException;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class TripPlannerBus extends Activity 
 {
@@ -41,5 +42,44 @@ public class TripPlannerBus extends Activity
      		
      		// do a query
     }	
+	
+	/**
+	 * This method populates the startingLocationSpinner and destinationSpinner with data from the database
+	 */
+	public void populateStartingAndDestination()
+	{
+		startingLocationSpinner = (Spinner) findViewById(R.id.startingLocationSpinner);  
+		destinationSpinner = (Spinner) findViewById(R.id.destinationSpinner);
+		List<String> locationList = new ArrayList<String>();
+		
+		/* Insert DB call to get all locations to populate Spinner */
+		
+		Cursor theCursor = database.geoAlarmDB.query("Routes", null, null, null, null, null, null);	
+		
+		if(theCursor != null)
+		{
+			theCursor.moveToFirst();
+							
+			Collections.addAll(locationList, theCursor.getColumnNames());
+			
+			
+			for(int i = 0; theCursor.isAfterLast() != false; i++)
+			{
+				int nameColumn = theCursor.getColumnIndex("name");
+				locationList.add(theCursor.getString(nameColumn));
+				
+				theCursor.moveToNext();
+			}
+			
+		}
+
+			theCursor.close();
+			database.geoAlarmDB.close();    
+													   
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getBaseContext(), android.R.layout.simple_spinner_item, locationList);		
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		startingLocationSpinner.setAdapter(adapter);				
+	}
 	
 }
